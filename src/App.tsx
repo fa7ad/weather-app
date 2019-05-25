4142454eddbc6319c674a30ed17c72118949cb17
Fahad Hossain <evilinfinite7@gmail.com>
2019-05-26 01:13:07 +0600

    create boilerplate with ts

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
+interface AppProps {
+  language: String;
+}
+
+const App: React.FC<AppProps> = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'>
+          Learn {props.language}
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
