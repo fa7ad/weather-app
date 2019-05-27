07543413538759d55948d55838f52963900fa7f8
Fahad Hossain <evilinfinite7@gmail.com>
2019-05-27 18:06:17 +0600

    add list of BD cities to initialState

--- src/reducers/location.ts
+++ src/reducers/location.ts
@@ -1,9 +1,33 @@
 import { createReducer } from 'redux-starter-kit';
 import { setLocation } from '../actions/location';
 
-export const location = createReducer('Dhaka,BD', {
+type Location = String | Number[];
+
+interface LocationState {
+  list: Location[];
+  current: Location;
+}
+
+const initialState: LocationState = {
+  list: [
+    'Dhaka, BD',
+    'Chittagong, BD',
+    'Khulna, BD',
+    'Sylhet, BD',
+    'Rajshahi, BD',
+    'Mymensingh, BD',
+    'Barisal, BD',
+    'Rangpur, BD',
+    'Comilla, BD',
+    'Narayanganj, BD',
+    'Gazipur, BD'
+  ],
+  current: 'Dhaka, BD'
+};
+
+export const location = createReducer(initialState, {
   [setLocation.type](state, action) {
-    Object.assign(state, action.payload);
+    state.current = action.payload;
   }
 });
 
