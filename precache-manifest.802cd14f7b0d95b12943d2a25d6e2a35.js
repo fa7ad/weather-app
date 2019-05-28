4cd927f91c973dcb07259ab0fbd8097dc7feed7f
Fahad Hossain <evilinfinite7@gmail.com>
2019-05-29 00:28:26 +0600

    Updates

---
+++ precache-manifest.802cd14f7b0d95b12943d2a25d6e2a35.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ed38188a9949dc91c0bb594fbde61d9",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "5cd1e628e69d4dbdaafa",
+    "url": "/weather-app/static/css/main.b4f5242f.chunk.css"
+  },
+  {
+    "revision": "bd1d4db44b54c83cf374",
+    "url": "/weather-app/static/js/2.169f224b.chunk.js"
+  },
+  {
+    "revision": "5cd1e628e69d4dbdaafa",
+    "url": "/weather-app/static/js/main.29143a14.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "b7d51e7a30a9285061f2b0ed513f81cd",
+    "url": "/weather-app/static/media/day.b7d51e7a.svg"
+  },
+  {
+    "revision": "2476e70c281731932f72bba7da6ea7ec",
+    "url": "/weather-app/static/media/night.2476e70c.svg"
+  },
+  {
+    "revision": "3d73387c2a1e21f207a8357724f536ba",
+    "url": "/weather-app/static/media/owfont-regular.3d73387c.ttf"
+  },
+  {
+    "revision": "e00c269cd4fb37b24eea2e4f37982c3d",
+    "url": "/weather-app/static/media/owfont-regular.e00c269c.eot"
+  },
+  {
+    "revision": "ed668bf0b123371be46d005b428f6188",
+    "url": "/weather-app/static/media/owfont-regular.ed668bf0.woff"
+  },
+  {
+    "revision": "f3ce0ef136eaaf2b1db22cf4069cb5dc",
+    "url": "/weather-app/static/media/owfont-regular.f3ce0ef1.svg"
+  }
+]);
