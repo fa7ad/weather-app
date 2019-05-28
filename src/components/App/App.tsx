0b3e2525c1f3fdc27ff37dde3c1b8d1b4be8e0e2
Fahad Hossain <evilinfinite7@gmail.com>
2019-05-29 00:34:07 +0600

    auto switch night/day

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import CurrentWeather from '../CurrentWeather';
 import Forecasts from '../Forecasts';
+import { connect } from 'react-redux';
+import { setTheme } from '../../actions';
 
-const App: React.FC = props => {
-  return (
-    <>
-      <CurrentWeather />
-      <Forecasts />
-    </>
-  );
-};
+class App extends React.PureComponent<{ setTheme: any }> {
+  componentDidMount() {
+    const now = new Date();
+    if (now.getHours() > 17 || now.getHours() < 5) {
+      this.props.setTheme('dark');
+    } else {
+      this.props.setTheme('light');
+    }
+  }
 
-export default App;
+  render() {
+    return (
+      <>
+        <CurrentWeather />
+        <Forecasts />
+      </>
+    );
+  }
+}
+
+export default connect(
+  null,
+  { setTheme }
+)(App);
